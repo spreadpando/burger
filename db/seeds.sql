26d6f8746cf5daa09345bddb62b846afb6549509
pando <spreadpando@gmail.com>
2019-06-23 23:47:57 -0600

    backend

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('BLT')
+;
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('CHIPOTLE RANCH')
+;
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('CHEESBURGER')
---
+++ models/burger.js
@@ -0,0 +1,28 @@
+var orm = require('../config/orm');
+
+var burger = {
+    all: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    deleteAll: function (condition, cb) {
+        orm.deleteAll("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
